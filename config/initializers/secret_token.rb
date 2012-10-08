624fe0de5b66447ffbb832047c2e9bef8059d538
cpyong27 <cpyong27@yahoo.com>
2012-10-09 00:44:53 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BearProj::Application.config.secret_token = '74a5c8967b77c743261305923cd0be568cc9e5c626b3e4e54de4e75bfd67fc292b81ced77c0b09519aad4f7d50a3b006f19119270e60fce683e851fea7ebe31c'
